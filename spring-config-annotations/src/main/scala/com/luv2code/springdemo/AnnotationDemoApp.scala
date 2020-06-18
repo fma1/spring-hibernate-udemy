e47a065d880d1ac8809e6e77cd5f91c3c06a51c8
Fred Ma <fredma1234@gmail.com>
2020-06-18 19:13:50 -0400

    Generated root pom.xml using Maven and modified packaging to pom Generated spring-config-annotations module using Maven Added Spring and Scala dependencies to root pom.xml Added trait Coach, class TennisCoach Added AnnotationDemoApp to get thatSillyCoach bean via ApplicationContext

---
+++ spring-config-annotations/src/main/scala/com/luv2code/springdemo/AnnotationDemoApp.scala
@@ -0,0 +1,18 @@
+package com.luv2code.springdemo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AnnotationDemoApp {
+
+}
+
+object AnnotationDemoApp extends App {
+  val context = SpringApplication.run(classOf[AnnotationDemoApp], args:_*)
+  val theCoach = context.getBean("thatSillyCoach", classOf[Coach])
+
+  println(theCoach.getDailyWorkout)
+
+  context.close()
+}
