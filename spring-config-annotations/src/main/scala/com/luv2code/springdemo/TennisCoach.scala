e89a972af27587f07262e896be8ed48920b2e06e
Fred Ma <fredma1234@gmail.com>
2020-06-18 19:31:58 -0400

    Modified TennisCoach to use constructor injection and take in a FortuneService

--- spring-config-annotations/src/main/scala/com/luv2code/springdemo/TennisCoach.scala
+++ spring-config-annotations/src/main/scala/com/luv2code/springdemo/TennisCoach.scala
@@ -1,8 +1,9 @@
 package com.luv2code.springdemo
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
-class TennisCoach extends Coach {
+class TennisCoach(@Autowired fortuneService: FortuneService) extends Coach {
   override def getDailyWorkout: String = "Practice your backend volley"
 }
