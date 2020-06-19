d670e16e2b0db86efd85f458b2131a9311df6d69
Fred Ma <fred.ma@blackrock.com>
2020-06-19 15:44:01 -0400

    Added SportConfig and changed AnnotationDemoApp to use AnnotationConfigApplicationContext

--- spring-config-annotations/src/main/scala/com/luv2code/springdemo/AnnotationDemoApp.scala
+++ spring-config-annotations/src/main/scala/com/luv2code/springdemo/AnnotationDemoApp.scala
@@ -1,15 +1,13 @@
 package com.luv2code.springdemo
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
-@SpringBootApplication
 class AnnotationDemoApp {
 
 }
 
 object AnnotationDemoApp extends App {
-  val context = SpringApplication.run(classOf[AnnotationDemoApp], args:_*)
+  val context = new AnnotationConfigApplicationContext(classOf[SportConfig])
   val theCoach = context.getBean("tennisCoach", classOf[Coach])
   val alphaCoach = context.getBean("tennisCoach", classOf[Coach])
 
---
+++ spring-config-annotations/src/main/scala/com/luv2code/springdemo/SportConfig.scala
@@ -0,0 +1,9 @@
+package com.luv2code.springdemo
+
+import org.springframework.context.annotation.{ComponentScan, Configuration}
+
+@ComponentScan
+@Configuration
+class SportConfig {
+
+}
