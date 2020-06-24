3937f9179e3774adf9cabeb6bead1bfc83473adf
Fred Ma <fred.ma@blackrock.com>
2020-06-24 15:53:38 -0400

    Modified DemoAppConfig to extend WebMvcConfigurer Overrided addResourcesHandler() method to add resource handler

--- spring-config-annotations/src/main/scala/com/luv2code/springdemo/config/DemoAppConfig.scala
+++ spring-config-annotations/src/main/scala/com/luv2code/springdemo/config/DemoAppConfig.scala
@@ -4,18 +4,22 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.ViewResolver
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.{EnableWebMvc, ResourceHandlerRegistry, WebMvcConfigurer}
 import org.springframework.web.servlet.view.InternalResourceViewResolver
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = Array("com.luv2code.springdemo"))
-class DemoAppConfig { // define a bean for ViewResolver
+class DemoAppConfig extends WebMvcConfigurer { // define a bean for ViewResolver
   @Bean
   def viewResolver: ViewResolver = {
     val viewResolver = new InternalResourceViewResolver()
     viewResolver.setPrefix("/web/view/")
     viewResolver.setSuffix(".jsp")
     viewResolver
+  }
+
+  override def addResourceHandlers(registry: ResourceHandlerRegistry): Unit = {
+    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/")
   }
 }
