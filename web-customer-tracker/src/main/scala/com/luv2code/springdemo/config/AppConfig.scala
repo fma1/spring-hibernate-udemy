77f1d43a5000760ea527257351ad26e36f5e6690
Fred Ma <fred.ma@blackrock.com>
2020-08-12 14:56:21 -0400

    Got Thymeleaf working with CSS by adding a few beans

--- web-customer-tracker/src/main/scala/com/luv2code/springdemo/config/AppConfig.scala
+++ web-customer-tracker/src/main/scala/com/luv2code/springdemo/config/AppConfig.scala
@@ -7,10 +7,55 @@ import org.springframework.context.annotation.{Bean, Configuration}
 import org.springframework.orm.hibernate5.{HibernateTransactionManager, LocalSessionFactoryBean}
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.annotation.EnableTransactionManagement
+import org.thymeleaf.spring5.SpringTemplateEngine
+import org.thymeleaf.spring5.view.ThymeleafViewResolver
+import org.thymeleaf.templatemode.TemplateMode
+import org.thymeleaf.templateresolver.{ClassLoaderTemplateResolver, ITemplateResolver}
 
 @Configuration
 @EnableTransactionManagement
 class AppConfig {
+  @Bean
+  def viewResolver: ThymeleafViewResolver = {
+    val viewResolver = new ThymeleafViewResolver
+    viewResolver.setTemplateEngine(templateEngine)
+    // NOTE 'order' and 'viewNames' are optional
+    viewResolver.setOrder(1)
+    viewResolver.setViewNames(Array[String](".html", ".xhtml"))
+    viewResolver
+  }
+
+  @Bean
+  def templateResolver: ITemplateResolver = { // SpringResourceTemplateResolver automatically integrates with Spring's own
+    // resource resolution infrastructure, which is highly recommended.
+    val templateResolver = new ClassLoaderTemplateResolver()
+    templateResolver.setPrefix("templates/")
+    templateResolver.setSuffix(".html")
+    // HTML is the default value, added here for the sake of clarity.
+    templateResolver.setTemplateMode(TemplateMode.HTML)
+    /*
+    // Template cache is true by default. Set to false if you want
+    // templates to be automatically updated when modified.
+    templateResolver.setCacheable(true)
+     */
+    templateResolver.setCacheable(false)
+    templateResolver
+  }
+
+  @Bean
+  def templateEngine: SpringTemplateEngine = { // SpringTemplateEngine automatically applies SpringStandardDialect and
+    // enables Spring's own MessageSource message resolution mechanisms.
+    val templateEngine = new SpringTemplateEngine
+    templateEngine.setTemplateResolver(templateResolver)
+    // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
+    // speed up execution in most scenarios, but might be incompatible
+    // with specific cases when expressions in one template are reused
+    // across different data types, so this flag is "false" by default
+    // for safer backwards compatibility.
+    templateEngine.setEnableSpringELCompiler(true)
+    templateEngine
+  }
+
   @Bean
   def sessionFactory(): LocalSessionFactoryBean = {
     val sessionFactory = new LocalSessionFactoryBean
--- web-customer-tracker/src/main/scala/com/luv2code/springdemo/config/WebMvcConfig.scala
+++ web-customer-tracker/src/main/scala/com/luv2code/springdemo/config/WebMvcConfig.scala
@@ -1,20 +1,18 @@
 package com.luv2code.springdemo.config
 
 import org.springframework.context.annotation.{Bean, ComponentScan, Configuration}
-import org.springframework.web.servlet.config.annotation.{DefaultServletHandlerConfigurer, EnableWebMvc, ViewResolverRegistry, WebMvcConfigurer, WebMvcConfigurerAdapter}
+import org.springframework.web.servlet.config.annotation.{DefaultServletHandlerConfigurer, EnableWebMvc, ResourceHandlerRegistry, ViewResolverRegistry, WebMvcConfigurer, WebMvcConfigurerAdapter}
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = Array("com.luv2code.springdemo.config"))
 class WebMvcConfig extends WebMvcConfigurer {
-
   override def configureDefaultServletHandling(configurer: DefaultServletHandlerConfigurer): Unit = {
     configurer.enable()
   }
 
-  /*
-  override def configureViewResolvers(registry: ViewResolverRegistry): Unit = {
-    registry.jsp("/", ".jsp")
+  override def addResourceHandlers(registry: ResourceHandlerRegistry): Unit = {
+    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/")
+    registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/")
   }
-   */
 }
