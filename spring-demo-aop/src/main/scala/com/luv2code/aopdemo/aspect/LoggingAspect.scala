c542f7b6811626af30911590de46b807aba5c55f
Fred Ma <fred.ma@blackrock.com>
2020-08-21 16:18:30 -0400

    Added functionality to read method arguments from JoinPoint

--- spring-demo-aop/src/main/scala/com/luv2code/aopdemo/aspect/LoggingAspect.scala
+++ spring-demo-aop/src/main/scala/com/luv2code/aopdemo/aspect/LoggingAspect.scala
@@ -1,7 +1,10 @@
 package com.luv2code.aopdemo.aspect
 
+import com.luv2code.aopdemo.entity.Account
 import org.apache.logging.log4j.{LogManager, Logger}
-import org.aspectj.lang.annotation.{Aspect, Before, Pointcut}
+import org.aspectj.lang.JoinPoint
+import org.aspectj.lang.annotation.{Aspect, Before}
+import org.aspectj.lang.reflect.MethodSignature
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 
@@ -12,7 +15,19 @@ class LoggingAspect {
   val logger: Logger = LogManager.getLogger(classOf[LoggingAspect])
 
   @Before("com.luv2code.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
-  def beforeAddAccountAdvice(): Unit = {
+  def beforeAddAccountAdvice(theJoinPoint: JoinPoint): Unit = {
     logger.info("=====>> Executing @Before advice on addAccount()")
+
+    val methodSignature = theJoinPoint.getSignature.asInstanceOf[MethodSignature]
+    logger.info(s"Method: $methodSignature")
+
+    theJoinPoint.getArgs.foreach(arg => {
+      logger.info(s"Method Argument: $arg")
+      arg match {
+        case account: Account =>
+          logger.info(s"account name: ${account.name}")
+          logger.info(s"account level: ${account.level}")
+      }
+    })
   }
 }
