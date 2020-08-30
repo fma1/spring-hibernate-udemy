5d72b2742d7a2a69e4b1f0be17315adab0047ebc
Fred Ma <fredma1234@gmail.com>
2020-08-30 17:25:53 -0400

    Created spring-rest-demo and added created Spring REST Controller Also added jackson-module-scala dependency

---
+++ spring-rest-demo/src/main/scala/com/luv2code/springdemo/rest/MyRestController.scala
@@ -0,0 +1,19 @@
+package com.luv2code.springdemo.rest
+
+import javax.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.{GetMapping, RequestMapping, ResponseBody, RestController}
+
+@Component
+@RestController
+@RequestMapping(Array("/test"))
+class MyRestController {
+
+  @GetMapping(path = Array("/hello"), produces = Array(MediaType.TEXT_PLAIN_VALUE))
+  def sayHello(response: HttpServletResponse): String = {
+    response.setContentType("text/plain")
+    response.setCharacterEncoding("UTF-8")
+    "Hello World!"
+  }
+}
