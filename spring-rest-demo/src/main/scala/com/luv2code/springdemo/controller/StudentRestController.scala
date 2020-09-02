5cf88232fd9151c976668b781dee7dda8e019b1f
Fred Ma <fred.ma@blackrock.com>
2020-09-02 14:55:07 -0400

    Added ExceptionHandlers to StudentRestController Added StudentErrorResponse and StudentNotFoundException

--- spring-rest-demo/src/main/scala/com/luv2code/springdemo/controller/StudentRestController.scala
+++ spring-rest-demo/src/main/scala/com/luv2code/springdemo/controller/StudentRestController.scala
@@ -1,9 +1,10 @@
 package com.luv2code.springdemo.controller
 
 import com.luv2code.springdemo.entity.Student
-import org.springframework.http.MediaType
+import com.luv2code.springdemo.exception.{StudentErrorResponse, StudentNotFoundException}
+import org.springframework.http.{HttpStatus, MediaType, ResponseEntity}
 import org.springframework.stereotype.Component
-import org.springframework.web.bind.annotation.{GetMapping, PathVariable, RequestMapping, RestController}
+import org.springframework.web.bind.annotation.{ExceptionHandler, GetMapping, PathVariable, RequestMapping, RestController}
 
 @Component
 @RestController
@@ -20,6 +21,21 @@ class StudentRestController {
 
   @GetMapping(path = Array("/students/{studentId}"), produces = Array(MediaType.APPLICATION_JSON_VALUE))
   def getStudent(@PathVariable studentId: Int): Student = {
-    theStudentsArray(studentId)
+    if (studentId < 0 || studentId > theStudentsArray.length)
+      throw new StudentNotFoundException(s"Student id not found - $studentId")
+    else
+      theStudentsArray(studentId)
+  }
+
+  @ExceptionHandler
+  def handleException(exception: StudentNotFoundException): ResponseEntity[StudentErrorResponse] = {
+     new ResponseEntity(
+       StudentErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getMessage, System.currentTimeMillis()), HttpStatus.NOT_FOUND)
+  }
+
+  @ExceptionHandler
+  def handleException(exception: Exception): ResponseEntity[StudentErrorResponse] = {
+    new ResponseEntity(
+      StudentErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage, System.currentTimeMillis()), HttpStatus.BAD_REQUEST)
   }
 }
---
+++ spring-rest-demo/src/main/scala/com/luv2code/springdemo/exception/StudentNotFoundException.scala
@@ -0,0 +1,3 @@
+package com.luv2code.springdemo.exception
+
+class StudentNotFoundException(message: String) extends RuntimeException(message)
