a3ab616f5b042ddb2bdc30897c32bea4d8d7456c
Fred Ma <fred.ma@blackrock.com>
2020-09-02 14:04:08 -0400

    Added getStudent to StudentRestController Added theStudents and theStudentsArray as lazy vals

--- spring-rest-demo/src/main/scala/com/luv2code/springdemo/controller/StudentRestController.scala
+++ spring-rest-demo/src/main/scala/com/luv2code/springdemo/controller/StudentRestController.scala
@@ -1,18 +1,25 @@
 package com.luv2code.springdemo.controller
 
 import com.luv2code.springdemo.entity.Student
-import javax.servlet.http.HttpServletResponse
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
-import org.springframework.web.bind.annotation.{GetMapping, RequestMapping, RestController}
+import org.springframework.web.bind.annotation.{GetMapping, PathVariable, RequestMapping, RestController}
 
 @Component
 @RestController
 @RequestMapping(Array("/api"))
 class StudentRestController {
 
+  lazy val theStudents = List(Student("Poornima", "Patel"), Student("Mario", "Rossi"), Student("Mary", "Smith"))
+  lazy val theStudentsArray: Array[Student] = theStudents.toArray
+
   @GetMapping(path = Array("/students"), produces = Array(MediaType.APPLICATION_JSON_VALUE))
-  def sayHello(response: HttpServletResponse): List[Student] = {
-    List(Student("Poornima", "Patel"), Student("Mario", "Rossi"), Student("Mary", "Smith"))
+  def getStudents: List[Student] = {
+    theStudents
+  }
+
+  @GetMapping(path = Array("/students/{studentId}"), produces = Array(MediaType.APPLICATION_JSON_VALUE))
+  def getStudent(@PathVariable studentId: Int): Student = {
+    theStudentsArray(studentId)
   }
 }
